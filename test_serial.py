845af3d9f48877a89342f6c4d5c3eb3dfa3fa487
Daniele Strigaro <daniele.strigaro@gmail.com>
2020-12-14 14:08:34 +0100

    simplified test_serial

--- test_serial.py
+++ test_serial.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python
 
-import configparser
-import sys
-import csv
-from io import StringIO
-from datetime import datetime, timedelta, timezone
-import json
-import os
-import time
-from lib.battery import read_ina219
 from lib.lopy import send_data
 
-# external lib
-import requests
-import yaml
-import paho.mqtt.client as mqtt
 
 send_data("ciaociao")
